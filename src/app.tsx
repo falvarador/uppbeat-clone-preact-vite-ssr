c5bebecc381008f3b5d490b9b56719fbb2d127c3
Freddy Alvarado R <alvaguez1990@gmail.com>
2023-04-10 13:21:40 -0600

    First commit on main

---
+++ src/app.tsx
@@ -0,0 +1,13 @@
+import { useState } from 'preact/hooks'
+import preactLogo from './assets/preact.svg'
+import viteLogo from '/vite.svg'
+
+export function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <h1 className="text-3xl font-bold underline">
+    Hello world!
+  </h1>
+  )
+}
